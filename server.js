03447bdb142700f174cb685ee37fc60fb60434fc
Jimmy Jam <jimmymano@mail.com>
2021-08-23 02:49:19 -0500

    updated /image route

--- server.js
+++ server.js
@@ -83,17 +83,13 @@ if(user.length){
 
 app.put('/image', (req,res)=>{
     const {id} = req.body;
-    let found = false;
-    database.users.forEach(user =>{
-    if(user.id === id){
-        found = true;
-        user.entries++
-        return res.json(user.entries);
-    }
+db('users').where('id','=',id)
+.increment('entries',1)
+.returning('entries')
+.then(entries=>{
+    res.json(entries[0]);
 })
-    if(!found){
-    res.status(400).json('not found');
-}
+.catch(err => res.status(400).json('error'))
 })
 
 app.listen('3000', ()=>{
