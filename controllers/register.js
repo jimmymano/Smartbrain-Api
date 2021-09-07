f04c154007e88a33ca47df57dece86ae0bdfbe45
Jimmy Jam <jimmymano@mail.com>
2021-09-06 19:00:48 -0500

    added validation to register route

--- controllers/register.js
+++ controllers/register.js
@@ -1,6 +1,9 @@
 
 const handleRegister = (req,res,db,bcrypt) =>{
     const{email,name,password}=req.body;
+    if(!email||!name||!password){
+       return res.status(400).json('incorrect form submission')
+    }
     const hash = bcrypt.hashSync(password,10);
 
    db.transaction(trx=>{
@@ -27,4 +30,3 @@ const handleRegister = (req,res,db,bcrypt) =>{
     }
 
 module.exports ={handleRegister:handleRegister}
-
