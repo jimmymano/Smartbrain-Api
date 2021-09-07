7cf8ba7920f82952eb60adff867e2708d53e8b67
Jimmy Jam <jimmymano@mail.com>
2021-09-07 13:49:56 -0500

    installed bcryptjs dependency and removed bcrypt dependency

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const knex = require('knex');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const saltRounds=10;
 
 //controllers
