15995d675e52a6832b272c671c01ed7d812a8b16
Jimmy Jam <jimmymano@mail.com>
2021-09-06 19:40:24 -0500

    updated validation

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,18 @@
+const { json } = require('body-parser');
+const Clarifai = require('clarifai')
+
+const app = new Clarifai.App({
+    apiKey: 'ec514acc370b4b3caa8fe2e9b1790573'
+   });
+const handleApiCall = (req,res)=>{
+    app.models.predict(Clarifai.FACE_DETECT_MODEL,req.body.input)
+    .then(data =>{
+        res.json(data);
+    })
+    .catch(err =>res.status(400).json('unable to work with api'))
+}
+
+
 const handleImage = (req,res,db)=>{
     const {id} = req.body;
 db('users').where('id','=',id)
@@ -9,4 +24,4 @@ db('users').where('id','=',id)
 .catch(err => res.status(400).json('error'))
 }
 
-module.exports = {handleImage:handleImage}
+module.exports = {handleImage:handleImage, handleApiCall:handleApiCall}
