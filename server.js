100b1b8c12742255045e77ada64faefacb99c664
Jimmy Jam <jimmymano@mail.com>
2021-09-07 22:09:23 -0500

    removed unnecessary code

--- server.js
+++ server.js
@@ -25,13 +25,6 @@ const db = knex({
 });
 
 const app = express();
-// app.all('*', function(req, res, next) {
-//     var origin = req.get('origin'); 
-//     res.header('Access-Control-Allow-Origin', origin);
-//     res.header("Access-Control-Allow-Headers", "X-Requested-With");
-//     res.header('Access-Control-Allow-Headers', 'Content-Type');
-//     next();
-// });
 
 app.use(bodyParser.json());
 app.use(cors());
